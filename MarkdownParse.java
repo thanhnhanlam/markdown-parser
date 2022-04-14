5ff0fb6d43118ffccf0db08166da201220ebc7c1
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2022-04-14 09:37:34 -0700

    MarkdownParse.java can accept file with brackets without links

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,11 +14,15 @@ public class MarkdownParse {
         while(currentIndex < markdown.length() && markdown.indexOf("[", currentIndex) != -1) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            
             int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (openParen == closeBracket + 1) {
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else {
+                currentIndex = closeBracket + 1;
+            }
         }
 
         return toReturn;
