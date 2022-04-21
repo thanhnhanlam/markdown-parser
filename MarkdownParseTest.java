0c715e52c2247129217c7c1e3183403a864d5038
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2022-04-21 09:29:38 -0700

    Add tests for test files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -22,4 +22,28 @@ public class MarkdownParseTest {
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
+
+    @Test
+    public void testBrackets() throws IOException {
+        List<String> testFileLinks = new ArrayList<>();
+        Path testFile = Path.of("test-brackets.md");
+        String testFileContent = Files.readString(testFile);
+        assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
+    }
+
+    @Test
+    public void testNewLine() throws IOException {
+        List<String> testFileLinks = List.of("https://google.com");
+        Path testFile = Path.of("test-new-line.md");
+        String testFileContent = Files.readString(testFile);
+        assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
+    }
+
+    @Test
+    public void testParentheses() throws IOException {
+        List<String> testFileLinks = List.of("http://en.wikipedia.org/wiki/PC_Tools_(Central_Point_Software");
+        Path testFile = Path.of("test-parentheses-in-link.md");
+        String testFileContent = Files.readString(testFile);
+        assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
+    }
 }
