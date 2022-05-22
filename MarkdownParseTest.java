ffcfa13a07b6725df62c342b8545992c9216942b
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2022-05-22 02:09:08 -0700

    Organize test files
    Move the test files of the repository into the test-files directory. Move the original content of test-files outside the test-files directory.
    Add a new directory lab-r-markdown-snippets.
    Add private final static variables to MarkdownParseTest.java to generalize the directories' name in the path.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,6 +11,9 @@ public class MarkdownParseTest {
 
     List<String> emptyString;
 
+    private final static String TEST_FOLDER = "test-files/";
+    private final static String LAB4_FOLDER = "lab-4-markdown-snippets/";
+
     @Before
     public void setUp() {
         emptyString = new ArrayList<>();
@@ -33,7 +36,7 @@ public class MarkdownParseTest {
     @Test
     public void testBrackets() throws IOException {
         List<String> testFileLinks = new ArrayList<>();
-        Path testFile = Path.of("test-brackets.md");
+        Path testFile = Path.of(LAB4_FOLDER + "test-brackets.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
@@ -41,7 +44,7 @@ public class MarkdownParseTest {
     @Test
     public void testNewLine() throws IOException {
         List<String> testFileLinks = List.of("https://google.com");
-        Path testFile = Path.of("test-extra-line.md");
+        Path testFile = Path.of(LAB4_FOLDER + "test-extra-line.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
@@ -49,7 +52,7 @@ public class MarkdownParseTest {
     @Test
     public void testParentheses() throws IOException {
         List<String> testFileLinks = List.of("http://en.wikipedia.org/wiki/PC_Tools_(Central_Point_Software)");
-        Path testFile = Path.of("test-parentheses-in-link.md");
+        Path testFile = Path.of(LAB4_FOLDER + "test-parentheses-in-link.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
@@ -57,7 +60,7 @@ public class MarkdownParseTest {
     @Test
     public void testFile1() throws IOException {
         List<String> testFileLinks = List.of("https://something.com", "some-thing.html");
-        Path testFile = Path.of("test-files/test-file.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
@@ -65,28 +68,28 @@ public class MarkdownParseTest {
     @Test
     public void testFile2() throws IOException {
         List<String> testFileLinks = List.of("https://something.com", "some-page.html");
-        Path testFile = Path.of("test-files/test-file2.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file2.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
 
     @Test
     public void testFile3() throws IOException {
-        Path testFile = Path.of("test-files/test-file3.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file3.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), emptyString);
     }
 
     @Test
     public void testFile4() throws IOException {
-        Path testFile = Path.of("test-files/test-file4.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file4.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), emptyString);
     }
 
     @Test
     public void testFile5() throws IOException {
-        Path testFile = Path.of("test-files/test-file5.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file5.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), emptyString);
     }
@@ -94,14 +97,14 @@ public class MarkdownParseTest {
     @Test
     public void testFile6() throws IOException {
         List<String> testFileLinks = List.of("page.com");
-        Path testFile = Path.of("test-files/test-file6.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file6.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
 
     @Test
     public void testFile7() throws IOException {
-        Path testFile = Path.of("test-files/test-file7.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file7.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), emptyString);
     }
@@ -109,7 +112,7 @@ public class MarkdownParseTest {
     @Test
     public void testFile8() throws IOException {
         List<String> testFileLinks = List.of("a link on the first line");
-        Path testFile = Path.of("test-files/test-file8.md");
+        Path testFile = Path.of(TEST_FOLDER + "test-file8.md");
         String testFileContent = Files.readString(testFile);
         assertEquals(MarkdownParse.getLinks(testFileContent), testFileLinks);
     }
