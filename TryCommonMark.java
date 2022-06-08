7c679b7ffcad22dbf341fc26d51600892f97b36a
thanhnhanlam <thanhnhan.lam15@gmail.com>
2022-06-07 23:57:01 -0700

    Add WordCountVisitor

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -3,11 +3,33 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 class TryCommonMark {
+
+
+
 	public static void main(String[] args) {
 		Parser parser = Parser.builder().build();
 		Node document = parser.parse("This is *Sparta*");
 		HtmlRenderer renderer = HtmlRenderer.builder().build();
 		renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
 		System.out.println(renderer);
+		// This part does the computation
+		Node node = parser.parse("Example\n========\n\nSome more text");
+		WordCountVisitor visitor = new WordCountVisitor();
+		node.accept(visitor);
+		System.out.println(visitor.wordCount); // 4
+	}
+}
+
+class WordCountVisitor extends AbstractVisitor {
+		
+	int wordCount = 0;
+
+	@Override
+	public void visit(Text text) {
+		// This is called for all Text nodes
+		// Count words
+		wordCount += text.getLiteral().split("\\W+").length;
+		// Descend into children
+		visitChildren(text);
 	}
 }
